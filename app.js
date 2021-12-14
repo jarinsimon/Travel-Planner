a3b1be7a88627f6d76ea559b4aaee814fad9a418
Jarin Simon <jarin.simon@gmail.com>
2021-12-14 00:13:55 -1000

    initial Amadeus integration

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ require("dotenv").config();
 const app = express();
 const PORT = 3000;
 
+const amadeus = new Amadeus({
+	clientId: process.env.API_KEY,
+	clientSecret: process.env.API_SECRET
+})
+
 var server = app.listen(PORT, function () {
 	console.log(`Server running on Port ${PORT}`);
 });
@@ -12,3 +17,18 @@ app.get('/', function (req, res){
 	res.send('Server is working');
 });
 
+app.get(`/citySearch`, async function(req, res){
+	console.log(req.query);
+	var keywords = req.query.keyword;
+	const response = await amadeus.referenceData.locations
+	.get({
+		keyword: keywords,
+		subType: 'CITY,AIRPORT',
+	})
+	.catch((x) => console.log(x));
+	try {
+		await res.json(JSON.parse(response.body));
+	} catch (err) {
+		await res.json(err);
+	}
+})
