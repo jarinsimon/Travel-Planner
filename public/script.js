f3f72dee2ae71da40c6786c9c3434ea0d3ffa2a0
Jarin Simon <jarin.simon@gmail.com>
2021-12-15 16:47:11 -1000

    Added event handlers in script.js, comments in index.html

--- public/script.js
+++ public/script.js
@@ -19,3 +19,34 @@ const reset = () => {
 	searchButton.disabled = true;
 };
 
+//Event handler to disable/enable search button based on completion of all search form values
+document.body.addEventListener("input", () => {
+	searchButton.disabled = !originInput.value || !destinationInput.value;
+});
+
+//Event handler to autocomplete origin input from Amadeus Airport/City Search API
+originInput.addEventListener("input", () => {
+	
+});
+
+//Event handler to autocomplete destination input from Amadeus Airport/City Search API
+destinationInput.addEventListener("input", () => {
+
+});
+
+//Event handler to show or hide return date input depending on one-way or round-trip flight plans
+flightTypeSelect.addEventListener("change", () => {
+	if (flightTypeSelect.value === "one-way") {
+		returnDate.classList.add("d-none");
+	} else {
+		returnDate.classList.remove("d-none");
+	}
+});
+
+//Searches for flight offers through Amadeus Flight Offers API
+searchButton.addEventListener("click", async() => {
+
+});
+
+//Ensures form is set to default
+reset();
